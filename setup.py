4190f47e6e2abf080e96a1e2fb9f337183f38d54
polarbean <harry.carey95@gmail.com>
2025-08-15 19:43:29 +0200

    update description and deploy to PyPi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="spatial_brain_maps",
     version="0.1.0",
-    description="Quantify expression in Allen atlas from slices",
+    description="Spatial brain maps is a tool for viewing 3D gene expression in the Mouse brain. by fetching registration data shared via EBRAINS and ISH data shared via the Allen Institute, spatial brain maps reconstructs the 3D patterns of gene expression in a standardised coordinate space. Data can be reconstructed as either a 3D volume, or point cloud.",
     packages=find_packages(),
     install_requires=[
         "pandas",
